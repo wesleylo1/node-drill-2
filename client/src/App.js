4e2208acb5d7f386ebbc02a9bbbeb1ad021fa53c
Wesley Lo <wesleylo1995@gmail.com>
2022-03-23 20:32:39 -0500

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,70 +1,74 @@
-import {useState, useEffect} from 'react';
-import './App.css';
-import axios from 'axios';
+import { useState, useEffect } from "react"
+import "./App.css"
+import axios from "axios"
 
 function App() {
+  const [input, setInput] = useState("")
+  const [serverInfo, setServerInfo] = useState([])
 
-  const [input, setInput] = useState('');
-  const [serverInfo, setServerInfo] = useState([]);
+  // handle input change
 
   function handleInput(e) {
-    setInput(e.target.value);
-  };
+    setInput(e.target.value)
+  }
+
+  // handle submit when clicked
 
   function handleSubmit(e) {
-    e.preventDefault();
+    e.preventDefault()
 
-    axios.post('http://localhost:3035/api/info', {
-      text: input
+    axios
+      .post("http://localhost:3035/api/info", {
+        text: input
+      })
+      .then((res) => {
+        setServerInfo(res.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+
+    console.log(input)
+    setInput("")
+  }
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3035/api/info")
+      .then((res) => {
+        console.log("RES", res.data)
+        setServerInfo(res.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [input])
+
+  // displays the array of input
+
+  function handleComponents() {
+    return serverInfo.map((item, index) => {
+      return (
+        <div key={index}>
+          <h1>{item.text}</h1>
+        </div>
+      )
     })
-    .then(res => {
-      setServerInfo(res.data);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-
-    console.log(input);
-    setInput('');
-  } 
-
- useEffect(() => {
-   
-  axios.get('http://localhost:3035/api/info')
-  .then(res => {
-    console.log('RES', res.data)
-    setServerInfo(res.data);
-  })
-  .catch(err => {
-    console.log(err);
-  });
-
- }, [input]);
-
- function handleComponents() {
-   return serverInfo.map((item, index) => {
-     return (
-       <div key={index}>
-         <h1>{item.text}</h1>
-       </div>
-     );
-   });
- }
+  }
 
   return (
-    <div className='App App-header'>
+    <div className="App App-header">
       <form onSubmit={handleSubmit}>
         <input onChange={handleInput} value={input}></input>
-        <button type='submit'>Submit</button>
+        <button type="submit">Submit</button>
       </form>
-        <br />
+      <br />
 
-        {handleComponents()}
+      {handleComponents()}
 
-        <br />
-     
+      <br />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- server/index.js
+++ server/index.js
@@ -1,14 +1,17 @@
 require("dotenv").config()
 const express = require("express")
-const app = express()
+const path = require("path")
 const cors = require("cors")
+const app = express()
+const port = process.env.PORT || 3035
 const { getInputs, createInput } = require("./controller")
 
+// Middleware
 app.use(express.json())
 app.use(cors())
 
+// Put endpoints here
 app.get("/api/info", getInputs)
 app.post("/api/info", createInput)
 
-const port = process.env.PORT || 3035
 app.listen(port, () => console.log(`server running on ${port}`))
